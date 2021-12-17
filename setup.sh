8cb161a4af53b67c5f3a9fe7c158bc0f8c5384c7
Valery Ivanov <ivalery111@gmail.com>
2021-12-17 21:42:13 +0300

    feat(setup.sh): add home-manager install option
    Signed-off-by: Valery Ivanov <ivalery111@gmail.com>

--- setup.sh
+++ setup.sh
@@ -8,9 +8,15 @@ function log_info {
     echo "[I]: $1"
 }
 
+function log_error {
+    echo "[E]: $1"
+    exit 1
+}
+
 function show_usage {
     echo "Usage:"
     echo "$0 partition [simple_mbr|simple_uefi] [disk-name]"
+    echo "$0 home-manager [install]"
     exit 1
 }
 
@@ -56,10 +62,10 @@ function __do_partition_simple_uefi {
     mkfs.ext4 -L NIXOS ${disk_name}1
     mkswap -L SWAP ${disk_name}2
     swapon ${disk_name}2
-    mkfs.fat -F 32 -n BOOT ${disk_name}3
+    mkfs.fat -F 32 -n boot ${disk_name}3
     mount /dev/disk/by-label/NIXOS /mnt
     mkdir -p /mnt/boot
-    mount /dev/disk/by-label/BOOT /mnt/boot
+    mount /dev/disk/by-label/boot /mnt/boot
     log_info "Partition done."
 
     print_post_partition_actions
@@ -83,10 +89,40 @@ function partition {
     esac
 }
 
+function __do_home_manager_install {
+    log_debug "__do_home_manager_install"
+
+    nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
+    nix-channel --update
+
+    log_info "You may need to log out"
+    export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+
+    log_info "Running the Home Manager installation and creating the first Home Manager generation"
+    nix-shell '<home-manager>' -A install
+    log_info "Done, Home Manager should be active"
+}
+
+function home-manager {
+    local command=$1
+
+    case $command in
+        "install")
+            __do_home_manager_install
+            ;;
+        *)
+            log_error "Unknown home-manager command"
+            ;;
+    esac
+}
+
 # Handle CLI arguments
 case $1 in
     "partition")
         partition $2 $3
+        ;;
+    "home-manager")
+        home-manager $2
         ;;
     *)
         show_usage
